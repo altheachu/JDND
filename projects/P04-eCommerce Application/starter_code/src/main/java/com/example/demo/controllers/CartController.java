533def3bda880e754c16c46ef1a14f0aeeff8a43
cch <aehtla2012@gmail.com>
2023-03-01 23:23:34 +0800

    [ADD] log

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/CartController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,8 @@ import com.example.demo.model.requests.ModifyCartRequest;
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
-	
+
+	private final static Logger log =  LoggerFactory.getLogger(CartController.class);
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -34,15 +37,22 @@ public class CartController {
 	
 	@PostMapping("/addToCart")
 	public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
+		log.info("request username",request.getUsername());
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
+			log.debug("found no user by username: {}", request.getUsername());
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+		log.info("request itemId",request.getItemId());
 		Optional<Item> item = itemRepository.findById(request.getItemId());
 		if(!item.isPresent()) {
+			log.debug("found no item by id: {}", request.getItemId());
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		Cart cart = user.getCart();
+		if(cart==null){
+			log.error("cart do not exist");
+		}
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.addItem(item.get()));
 		cartRepository.save(cart);
@@ -51,15 +61,22 @@ public class CartController {
 	
 	@PostMapping("/removeFromCart")
 	public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
+		log.info("request username",request.getUsername());
 		User user = userRepository.findByUsername(request.getUsername());
 		if(user == null) {
+			log.debug("found no user by username: {}", request.getUsername());
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+		log.info("request itemId",request.getItemId());
 		Optional<Item> item = itemRepository.findById(request.getItemId());
 		if(!item.isPresent()) {
+			log.debug("found no item by id: {}", request.getItemId());
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 		Cart cart = user.getCart();
+		if(cart==null){
+			log.error("cart do not exist");
+		}
 		IntStream.range(0, request.getQuantity())
 			.forEach(i -> cart.removeItem(item.get()));
 		cartRepository.save(cart);
